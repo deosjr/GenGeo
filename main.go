324285445a50da86bd5b5797ff0d9a2d3f42d478
Sjoerd Dost <sdost@ebay.com>
2018-09-22 12:25:06 +0200

    parametric object function

--- main.go
+++ main.go
@@ -28,14 +28,6 @@ func main() {
 
 	m.SetBackgroundColor(m.NewColor(50, 100, 150))
 
-	// diffMat := &m.DiffuseMaterial{m.NewColor(50, 10, 100)}
-	// reflMat := &m.ReflectiveMaterial{scene}
-	translation := m.Translate(m.Vector{1, 0, 2})
-	rotation := m.RotateY(math.Pi)
-
-	numPoints := 20
-	radius := 0.5
-
 	unitcircle := parametricFunction{
 		x: func(t float64) float64 { return math.Cos(t) },
 		y: func(t float64) float64 { return math.Sin(t) },
@@ -54,21 +46,15 @@ func main() {
 		z: func(t float64) float64 { return 0.0 },
 	}
 
-	f := frenetFrame(unitcircle, unitcircleDeriv, unitcircle2ndDeriv)
-	p0, _, n0, b0 := f(0)
-	p1, _, n1, b1 := f(math.Pi / 8.0)
-	p2, _, n2, b2 := f(math.Pi / 4.0)
-	p3, _, n3, b3 := f(3 * math.Pi / 8.0)
-	p4, _, n4, b4 := f(math.Pi / 2.0)
+	numPoints := 100
+	radius := 0.5
+	numSteps := 100
+	stepSize := math.Pi / float64(2*(numSteps-1))
+	diffMat := &m.DiffuseMaterial{m.NewColor(50, 150, 80)}
+	complexObject := parametricObject(unitcircle, unitcircleDeriv, unitcircle2ndDeriv, numPoints, radius, numSteps, stepSize, diffMat)
 
-	c0 := pointsOnCircle(p0, n0, b0, numPoints, radius)
-	c1 := pointsOnCircle(p1, n1, b1, numPoints, radius)
-	c2 := pointsOnCircle(p2, n2, b2, numPoints, radius)
-	c3 := pointsOnCircle(p3, n3, b3, numPoints, radius)
-	c4 := pointsOnCircle(p4, n4, b4, numPoints, radius)
-
-	triangles := joinCirclePoints([][]m.Vector{c4, c3, c2, c1, c0})
-	complexObject := m.NewComplexObject(triangles)
+	translation := m.Translate(m.Vector{1, 0, 2})
+	rotation := m.RotateY(math.Pi)
 	boom := m.NewSharedObject(complexObject, translation.Mul(rotation))
 	scene.Add(boom)
 
@@ -80,70 +66,4 @@ func main() {
 	camera.LookAt(from, to, ey)
 	film := render.Render(scene, numWorkers)
 	film.SaveAsPNG("out.png")
-}
-
-func pointsOnCircle(p, normal, binormal m.Vector, numPoints int, radius float64) []m.Vector {
-	angle := (1 / (float64(numPoints))) * (2 * math.Pi)
-	l := make([]m.Vector, numPoints)
-	for i := 0; i < numPoints; i++ {
-		xVector := normal.Times(radius * math.Cos(float64(i)*angle))
-		yVector := binormal.Times(radius * math.Sin(float64(i)*angle))
-		newP := p.Add(xVector).Add(yVector)
-		l[i] = newP
-	}
-	return l
-}
-
-// assumes each list has the same number of points
-func joinCirclePoints(pointLists [][]m.Vector) []m.Object {
-	diffMat := &m.DiffuseMaterial{m.NewColor(50, 200, 100)}
-
-	numLists := len(pointLists)
-	numPoints := len(pointLists[0])
-	triangles := make([]m.Object, 2*numPoints*(numLists-1))
-
-	for i := 0; i < numLists-1; i++ {
-		offset := 2 * numPoints * i
-		c1 := pointLists[i]
-		c2 := pointLists[i+1]
-
-		triangles[offset] = m.NewTriangle(c1[numPoints-1], c1[0], c2[numPoints-1], diffMat)
-		triangles[offset+1] = m.NewTriangle(c2[numPoints-1], c1[0], c2[0], diffMat)
-		for j := 0; j < numPoints-1; j++ {
-			triangles[offset+(j+1)*2] = m.NewTriangle(c1[j], c1[j+1], c2[j], diffMat)
-			triangles[offset+(j+1)*2+1] = m.NewTriangle(c2[j], c1[j+1], c2[j+1], diffMat)
-		}
-	}
-
-	return triangles
-}
-
-type parametricFunction struct {
-	x func(t float64) float64
-	y func(t float64) float64
-	z func(t float64) float64
-}
-
-func (f parametricFunction) Vector(t float64) m.Vector {
-	v := m.Vector{f.x(t), f.y(t), f.z(t)}
-	return v.Normalize()
-}
-
-func frenetFrame(f1, f2, f3 parametricFunction) func(t float64) (p, tangent, normal, binormal m.Vector) {
-	frenet1 := f2.Vector
-	frenet2 := func(t float64) m.Vector {
-		secondDeriv := f3.Vector(t)
-		e1 := frenet1(t)
-		return secondDeriv.Sub(e1.Times(secondDeriv.Dot(e1)))
-	}
-	frenet3 := func(t float64) m.Vector {
-		return frenet1(t).Cross(frenet2(t))
-	}
-	return func(t float64) (p, tangent, normal, binormal m.Vector) {
-		p = f1.Vector(t)
-		tangent = frenet1(t)
-		normal = frenet2(t)
-		binormal = frenet3(t)
-		return p, tangent, normal, binormal
-	}
 }
---
+++ parametric.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"math"
+
+	m "github.com/deosjr/GRayT/src/model"
+)
+
+type parametricFunction struct {
+	x func(t float64) float64
+	y func(t float64) float64
+	z func(t float64) float64
+}
+
+func (f parametricFunction) Vector(t float64) m.Vector {
+	v := m.Vector{f.x(t), f.y(t), f.z(t)}
+	return v.Normalize()
+}
+
+func frenetFunctions(f, deriv, secDeriv parametricFunction) func(t float64) (p, tangent, normal, binormal m.Vector) {
+	frenet1 := deriv.Vector
+	frenet2 := func(t float64) m.Vector {
+		secondDeriv := secDeriv.Vector(t)
+		e1 := frenet1(t)
+		return secondDeriv.Sub(e1.Times(secondDeriv.Dot(e1)))
+	}
+	frenet3 := func(t float64) m.Vector {
+		return frenet1(t).Cross(frenet2(t))
+	}
+	return func(t float64) (p, tangent, normal, binormal m.Vector) {
+		p = f.Vector(t)
+		tangent = frenet1(t)
+		normal = frenet2(t)
+		binormal = frenet3(t)
+		return p, tangent, normal, binormal
+	}
+}
+
+func pointsOnCircle(p, normal, binormal m.Vector, numPoints int, radius float64) []m.Vector {
+	angle := (1 / (float64(numPoints))) * (2 * math.Pi)
+	l := make([]m.Vector, numPoints)
+	for i := 0; i < numPoints; i++ {
+		xVector := normal.Times(radius * math.Cos(float64(i)*angle))
+		yVector := binormal.Times(radius * math.Sin(float64(i)*angle))
+		newP := p.Add(xVector).Add(yVector)
+		l[i] = newP
+	}
+	return l
+}
+
+// assumes each list has the same number of points
+func joinCirclePoints(pointLists [][]m.Vector, mat m.Material) []m.Object {
+	numLists := len(pointLists)
+	numPoints := len(pointLists[0])
+	triangles := make([]m.Object, 2*numPoints*(numLists-1))
+
+	for i := 0; i < numLists-1; i++ {
+		offset := 2 * numPoints * i
+		c1 := pointLists[i]
+		c2 := pointLists[i+1]
+
+		triangles[offset] = m.NewTriangle(c1[numPoints-1], c1[0], c2[numPoints-1], mat)
+		triangles[offset+1] = m.NewTriangle(c2[numPoints-1], c1[0], c2[0], mat)
+		for j := 0; j < numPoints-1; j++ {
+			triangles[offset+(j+1)*2] = m.NewTriangle(c1[j], c1[j+1], c2[j], mat)
+			triangles[offset+(j+1)*2+1] = m.NewTriangle(c2[j], c1[j+1], c2[j+1], mat)
+		}
+	}
+
+	return triangles
+}
+
+// TODO: cleanup parameters
+func parametricObject(f1, f2, f3 parametricFunction, numPoints int, radius float64, numSteps int, stepSize float64, mat m.Material) m.Object {
+	f := frenetFunctions(f1, f2, f3)
+	points := make([][]m.Vector, numSteps)
+
+	for i := 0; i < numSteps; i++ {
+		t := float64(i) * stepSize
+		p, _, normal, binormal := f(t)
+		// reverse index because of triangle normals?
+		points[numSteps-1-i] = pointsOnCircle(p, normal, binormal, numPoints, radius)
+	}
+
+	triangles := joinCirclePoints(points, mat)
+	return m.NewComplexObject(triangles)
+}
